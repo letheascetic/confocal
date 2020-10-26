a158cd1d90eee2f7c93c3f218e5e9af7be7c789f
tangmb757 <tangmb@zhejianglab.com>
2020-10-26 22:22:53 +0800

    update

--- code/confocal_ui/confocal_ui/FormMeas.Designer.cs
+++ code/confocal_ui/confocal_ui/FormMeas.Designer.cs
@@ -28,27 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMeas));
             this.pnlMeas = new System.Windows.Forms.Panel();
-            this.pnlHistogram = new System.Windows.Forms.Panel();
-            this.pnlHisBottom = new System.Windows.Forms.Panel();
-            this.chartHistogram = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.cbxHSelect = new System.Windows.Forms.ComboBox();
             this.pnlScale = new System.Windows.Forms.Panel();
+            this.chartScale = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.pblScaleBottom = new System.Windows.Forms.Panel();
             this.cbxSSelect = new System.Windows.Forms.ComboBox();
-            this.chartScale = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pnlHistogram = new System.Windows.Forms.Panel();
+            this.chartHistogram = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pnlHisBottom = new System.Windows.Forms.Panel();
+            this.cbxHSelect = new System.Windows.Forms.ComboBox();
             this.pnlMeas.SuspendLayout();
-            this.pnlHistogram.SuspendLayout();
-            this.pnlHisBottom.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.chartHistogram)).BeginInit();
             this.pnlScale.SuspendLayout();
-            this.pblScaleBottom.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chartScale)).BeginInit();
+            this.pblScaleBottom.SuspendLayout();
+            this.pnlHistogram.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartHistogram)).BeginInit();
+            this.pnlHisBottom.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlMeas
@@ -59,78 +59,44 @@
             this.pnlMeas.Controls.Add(this.pnlHistogram);
             this.pnlMeas.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlMeas.Location = new System.Drawing.Point(0, 0);
-            this.pnlMeas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlMeas.Name = "pnlMeas";
-            this.pnlMeas.Size = new System.Drawing.Size(381, 734);
+            this.pnlMeas.Size = new System.Drawing.Size(285, 587);
             this.pnlMeas.TabIndex = 0;
-            // 
-            // pnlHistogram
-            // 
-            this.pnlHistogram.Controls.Add(this.chartHistogram);
-            this.pnlHistogram.Controls.Add(this.pnlHisBottom);
-            this.pnlHistogram.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlHistogram.Location = new System.Drawing.Point(0, 0);
-            this.pnlHistogram.Margin = new System.Windows.Forms.Padding(0);
-            this.pnlHistogram.Name = "pnlHistogram";
-            this.pnlHistogram.Size = new System.Drawing.Size(381, 165);
-            this.pnlHistogram.TabIndex = 1;
-            // 
-            // pnlHisBottom
-            // 
-            this.pnlHisBottom.Controls.Add(this.cbxHSelect);
-            this.pnlHisBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pnlHisBottom.Location = new System.Drawing.Point(0, 124);
-            this.pnlHisBottom.Margin = new System.Windows.Forms.Padding(0);
-            this.pnlHisBottom.Name = "pnlHisBottom";
-            this.pnlHisBottom.Size = new System.Drawing.Size(381, 41);
-            this.pnlHisBottom.TabIndex = 3;
-            // 
-            // chartHistogram
-            // 
-            chartArea2.Name = "ChartArea1";
-            this.chartHistogram.ChartAreas.Add(chartArea2);
-            this.chartHistogram.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.chartHistogram.Location = new System.Drawing.Point(0, 0);
-            this.chartHistogram.Margin = new System.Windows.Forms.Padding(4);
-            this.chartHistogram.Name = "chartHistogram";
-            series2.ChartArea = "ChartArea1";
-            series2.Name = "Series1";
-            series2.YValuesPerPoint = 4;
-            this.chartHistogram.Series.Add(series2);
-            this.chartHistogram.Size = new System.Drawing.Size(381, 124);
-            this.chartHistogram.TabIndex = 4;
-            this.chartHistogram.Text = "chart1";
-            // 
-            // cbxHSelect
-            // 
-            this.cbxHSelect.FormattingEnabled = true;
-            this.cbxHSelect.Items.AddRange(new object[] {
-            "X方向",
-            "Y方向",
-            "自定义"});
-            this.cbxHSelect.Location = new System.Drawing.Point(12, 7);
-            this.cbxHSelect.Name = "cbxHSelect";
-            this.cbxHSelect.Size = new System.Drawing.Size(100, 23);
-            this.cbxHSelect.TabIndex = 0;
             // 
             // pnlScale
             // 
             this.pnlScale.Controls.Add(this.chartScale);
             this.pnlScale.Controls.Add(this.pblScaleBottom);
             this.pnlScale.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlScale.Location = new System.Drawing.Point(0, 165);
+            this.pnlScale.Location = new System.Drawing.Point(0, 132);
+            this.pnlScale.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pnlScale.Name = "pnlScale";
-            this.pnlScale.Size = new System.Drawing.Size(381, 187);
+            this.pnlScale.Size = new System.Drawing.Size(285, 150);
             this.pnlScale.TabIndex = 2;
+            // 
+            // chartScale
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartScale.ChartAreas.Add(chartArea1);
+            this.chartScale.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartScale.Location = new System.Drawing.Point(0, 0);
+            this.chartScale.Name = "chartScale";
+            series1.ChartArea = "ChartArea1";
+            series1.Name = "Series1";
+            series1.YValuesPerPoint = 4;
+            this.chartScale.Series.Add(series1);
+            this.chartScale.Size = new System.Drawing.Size(285, 117);
+            this.chartScale.TabIndex = 5;
+            this.chartScale.Text = "chart1";
             // 
             // pblScaleBottom
             // 
             this.pblScaleBottom.Controls.Add(this.cbxSSelect);
             this.pblScaleBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pblScaleBottom.Location = new System.Drawing.Point(0, 146);
+            this.pblScaleBottom.Location = new System.Drawing.Point(0, 117);
             this.pblScaleBottom.Margin = new System.Windows.Forms.Padding(0);
             this.pblScaleBottom.Name = "pblScaleBottom";
-            this.pblScaleBottom.Size = new System.Drawing.Size(381, 41);
+            this.pblScaleBottom.Size = new System.Drawing.Size(285, 33);
             this.pblScaleBottom.TabIndex = 4;
             // 
             // cbxSSelect
@@ -140,51 +106,85 @@
             "X方向",
             "Y方向",
             "自定义"});
-            this.cbxSSelect.Location = new System.Drawing.Point(12, 7);
+            this.cbxSSelect.Location = new System.Drawing.Point(9, 6);
+            this.cbxSSelect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cbxSSelect.Name = "cbxSSelect";
-            this.cbxSSelect.Size = new System.Drawing.Size(100, 23);
+            this.cbxSSelect.Size = new System.Drawing.Size(76, 20);
             this.cbxSSelect.TabIndex = 0;
             // 
-            // chartScale
+            // pnlHistogram
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chartScale.ChartAreas.Add(chartArea1);
-            this.chartScale.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.chartScale.Location = new System.Drawing.Point(0, 0);
-            this.chartScale.Margin = new System.Windows.Forms.Padding(4);
-            this.chartScale.Name = "chartScale";
-            series1.ChartArea = "ChartArea1";
-            series1.Name = "Series1";
-            series1.YValuesPerPoint = 4;
-            this.chartScale.Series.Add(series1);
-            this.chartScale.Size = new System.Drawing.Size(381, 146);
-            this.chartScale.TabIndex = 5;
-            this.chartScale.Text = "chart1";
+            this.pnlHistogram.Controls.Add(this.chartHistogram);
+            this.pnlHistogram.Controls.Add(this.pnlHisBottom);
+            this.pnlHistogram.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHistogram.Location = new System.Drawing.Point(0, 0);
+            this.pnlHistogram.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlHistogram.Name = "pnlHistogram";
+            this.pnlHistogram.Size = new System.Drawing.Size(285, 132);
+            this.pnlHistogram.TabIndex = 1;
+            // 
+            // chartHistogram
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chartHistogram.ChartAreas.Add(chartArea2);
+            this.chartHistogram.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartHistogram.Location = new System.Drawing.Point(0, 0);
+            this.chartHistogram.Name = "chartHistogram";
+            series2.ChartArea = "ChartArea1";
+            series2.Name = "Series1";
+            series2.YValuesPerPoint = 4;
+            this.chartHistogram.Series.Add(series2);
+            this.chartHistogram.Size = new System.Drawing.Size(285, 99);
+            this.chartHistogram.TabIndex = 4;
+            this.chartHistogram.Text = "chart1";
+            // 
+            // pnlHisBottom
+            // 
+            this.pnlHisBottom.Controls.Add(this.cbxHSelect);
+            this.pnlHisBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlHisBottom.Location = new System.Drawing.Point(0, 99);
+            this.pnlHisBottom.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlHisBottom.Name = "pnlHisBottom";
+            this.pnlHisBottom.Size = new System.Drawing.Size(285, 33);
+            this.pnlHisBottom.TabIndex = 3;
+            // 
+            // cbxHSelect
+            // 
+            this.cbxHSelect.FormattingEnabled = true;
+            this.cbxHSelect.Items.AddRange(new object[] {
+            "X方向",
+            "Y方向",
+            "自定义"});
+            this.cbxHSelect.Location = new System.Drawing.Point(9, 6);
+            this.cbxHSelect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbxHSelect.Name = "cbxHSelect";
+            this.cbxHSelect.Size = new System.Drawing.Size(76, 20);
+            this.cbxHSelect.TabIndex = 0;
             // 
             // FormMeas
             // 
             this.AutoHidePortion = 0.4D;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(381, 734);
+            this.ClientSize = new System.Drawing.Size(285, 587);
             this.Controls.Add(this.pnlMeas);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.HideOnClose = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormMeas";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "测量和调整";
             this.pnlMeas.ResumeLayout(false);
-            this.pnlHistogram.ResumeLayout(false);
-            this.pnlHisBottom.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.chartHistogram)).EndInit();
             this.pnlScale.ResumeLayout(false);
-            this.pblScaleBottom.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.chartScale)).EndInit();
+            this.pblScaleBottom.ResumeLayout(false);
+            this.pnlHistogram.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chartHistogram)).EndInit();
+            this.pnlHisBottom.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
