6ba422b343750e3eca7e0b11567e8edad1793455
tangmb757 <tangmb@zhejianglab.com>
2020-12-16 13:51:51 +0800

    update

---
+++ code/confocal/confocal_ui/FormMain.cs
@@ -0,0 +1,38 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+namespace confocal_ui
+{
+    public partial class FormMain : Form
+    {
+        /************************************************************************************/
+        private static readonly ILog Logger = log4net.LogManager.GetLogger("info");
+        /************************************************************************************/
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void InitControlers()
+        {
+            // version
+            string version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            this.Text += version;
+            Logger.Info(string.Format("get software version: [{0}]", version));
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            InitControlers();
+        }
+    }
+}
--- code/confocal/confocal_ui/Properties/AssemblyInfo.cs
+++ code/confocal/confocal_ui/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("confocal_ui")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Confocal Imaging System")]
+[assembly: AssemblyDescription("A confocal imaging visualization software")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("confocal_ui")]
+[assembly: AssemblyCompany("ZJU OPT")]
+[assembly: AssemblyProduct("Confocal Imaging")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,8 +31,7 @@ using System.Runtime.InteropServices;
 //
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", ConfigFileExtension = "config", Watch = true)]
 
