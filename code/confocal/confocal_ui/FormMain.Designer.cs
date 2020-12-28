4b2cf61c76e675afe99ef6595cdf728e67b824a2
tangmb757 <tangmb@zhejianglab.com>
2020-12-28 16:02:56 +0800

    update

--- code/confocal/confocal_ui/FormMain.Designer.cs
+++ code/confocal/confocal_ui/FormMain.Designer.cs
@@ -29,62 +29,90 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
-            this.statusStrip = new System.Windows.Forms.StatusStrip();
-            this.menuStrip = new System.Windows.Forms.MenuStrip();
-            this.toolStrip = new System.Windows.Forms.ToolStrip();
-            this.dockLeft = new C1.Win.C1Command.C1CommandDock();
+            this.mainMenu = new C1.Win.C1Command.C1MainMenu();
+            this.cmdHolder = new C1.Win.C1Command.C1CommandHolder();
+            this.c1CommandLink1 = new C1.Win.C1Command.C1CommandLink();
+            this.dockToolBar = new C1.Win.C1Command.C1CommandDock();
+            this.toolBar = new C1.Win.C1Command.C1ToolBar();
+            this.c1CommandLink2 = new C1.Win.C1Command.C1CommandLink();
+            this.dockMain = new C1.Win.C1Command.C1DockingTab();
             this.dockBottom = new C1.Win.C1Command.C1CommandDock();
             this.dockTabOutput = new C1.Win.C1Command.C1DockingTab();
             this.tpgLog = new C1.Win.C1Command.C1DockingTabPage();
             this.textLog = new System.Windows.Forms.TextBox();
+            this.dockLeft = new C1.Win.C1Command.C1CommandDock();
             this.dockTabScan = new C1.Win.C1Command.C1DockingTab();
             this.tpgScanSettings = new C1.Win.C1Command.C1DockingTabPage();
             this.tpgScanField = new C1.Win.C1Command.C1DockingTabPage();
-            this.dockMain = new C1.Win.C1Command.C1DockingTab();
-            ((System.ComponentModel.ISupportInitialize)(this.dockLeft)).BeginInit();
-            this.dockLeft.SuspendLayout();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            ((System.ComponentModel.ISupportInitialize)(this.cmdHolder)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dockToolBar)).BeginInit();
+            this.dockToolBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dockMain)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dockBottom)).BeginInit();
             this.dockBottom.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dockTabOutput)).BeginInit();
             this.dockTabOutput.SuspendLayout();
             this.tpgLog.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dockLeft)).BeginInit();
+            this.dockLeft.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dockTabScan)).BeginInit();
             this.dockTabScan.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dockMain)).BeginInit();
             this.SuspendLayout();
             // 
-            // statusStrip
+            // mainMenu
             // 
-            this.statusStrip.Location = new System.Drawing.Point(0, 739);
-            this.statusStrip.Name = "statusStrip";
-            this.statusStrip.Size = new System.Drawing.Size(984, 22);
-            this.statusStrip.TabIndex = 0;
-            this.statusStrip.Text = "statusStrip1";
+            this.mainMenu.AccessibleName = "Menu Bar";
+            this.mainMenu.CommandHolder = this.cmdHolder;
+            this.mainMenu.CommandLinks.AddRange(new C1.Win.C1Command.C1CommandLink[] {
+            this.c1CommandLink1});
+            this.mainMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(984, 20);
             // 
-            // menuStrip
+            // cmdHolder
             // 
-            this.menuStrip.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip.Name = "menuStrip";
-            this.menuStrip.Size = new System.Drawing.Size(984, 24);
-            this.menuStrip.TabIndex = 1;
-            this.menuStrip.Text = "menuStrip1";
+            this.cmdHolder.Owner = this;
             // 
-            // toolStrip
+            // c1CommandLink1
             // 
-            this.toolStrip.Location = new System.Drawing.Point(0, 24);
-            this.toolStrip.Name = "toolStrip";
-            this.toolStrip.Size = new System.Drawing.Size(984, 25);
-            this.toolStrip.TabIndex = 2;
-            this.toolStrip.Text = "toolStrip1";
+            this.c1CommandLink1.Text = "新命令";
             // 
-            // dockLeft
+            // dockToolBar
             // 
-            this.dockLeft.Controls.Add(this.dockTabScan);
-            this.dockLeft.Dock = System.Windows.Forms.DockStyle.Right;
-            this.dockLeft.Id = 3;
-            this.dockLeft.Location = new System.Drawing.Point(751, 49);
-            this.dockLeft.Name = "dockLeft";
-            this.dockLeft.Size = new System.Drawing.Size(233, 690);
+            this.dockToolBar.Controls.Add(this.toolBar);
+            this.dockToolBar.Id = 3;
+            this.dockToolBar.Location = new System.Drawing.Point(0, 20);
+            this.dockToolBar.Name = "dockToolBar";
+            this.dockToolBar.Size = new System.Drawing.Size(984, 26);
+            // 
+            // toolBar
+            // 
+            this.toolBar.CommandHolder = this.cmdHolder;
+            this.toolBar.CommandLinks.AddRange(new C1.Win.C1Command.C1CommandLink[] {
+            this.c1CommandLink2});
+            this.toolBar.Location = new System.Drawing.Point(3, 0);
+            this.toolBar.Name = "toolBar";
+            this.toolBar.Size = new System.Drawing.Size(29, 24);
+            this.toolBar.Text = "c1ToolBar1";
+            // 
+            // c1CommandLink2
+            // 
+            this.c1CommandLink2.Text = "新命令";
+            // 
+            // dockMain
+            // 
+            this.dockMain.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.dockMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dockMain.CanCloseTabs = true;
+            this.dockMain.CanMoveTabs = true;
+            this.dockMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dockMain.Location = new System.Drawing.Point(0, 46);
+            this.dockMain.Name = "dockMain";
+            this.dockMain.Size = new System.Drawing.Size(751, 555);
+            this.dockMain.TabIndex = 11;
+            this.dockMain.TabsSpacing = 0;
             // 
             // dockBottom
             // 
@@ -133,6 +161,15 @@
             this.textLog.Size = new System.Drawing.Size(751, 89);
             this.textLog.TabIndex = 0;
             // 
+            // dockLeft
+            // 
+            this.dockLeft.Controls.Add(this.dockTabScan);
+            this.dockLeft.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dockLeft.Id = 3;
+            this.dockLeft.Location = new System.Drawing.Point(751, 46);
+            this.dockLeft.Name = "dockLeft";
+            this.dockLeft.Size = new System.Drawing.Size(233, 693);
+            // 
             // dockTabScan
             // 
             this.dockTabScan.Alignment = System.Windows.Forms.TabAlignment.Bottom;
@@ -146,7 +183,7 @@
             this.dockTabScan.Name = "dockTabScan";
             this.dockTabScan.SelectedIndex = 1;
             this.dockTabScan.ShowCaption = true;
-            this.dockTabScan.Size = new System.Drawing.Size(233, 690);
+            this.dockTabScan.Size = new System.Drawing.Size(233, 693);
             this.dockTabScan.TabIndex = 0;
             this.dockTabScan.TabSizeMode = C1.Win.C1Command.TabSizeModeEnum.Fit;
             this.dockTabScan.TabsSpacing = 0;
@@ -156,7 +193,7 @@
             this.tpgScanSettings.CaptionVisible = true;
             this.tpgScanSettings.Location = new System.Drawing.Point(3, 0);
             this.tpgScanSettings.Name = "tpgScanSettings";
-            this.tpgScanSettings.Size = new System.Drawing.Size(230, 667);
+            this.tpgScanSettings.Size = new System.Drawing.Size(230, 670);
             this.tpgScanSettings.TabIndex = 0;
             this.tpgScanSettings.Text = "Scan Settings";
             // 
@@ -165,22 +202,17 @@
             this.tpgScanField.CaptionVisible = true;
             this.tpgScanField.Location = new System.Drawing.Point(3, 0);
             this.tpgScanField.Name = "tpgScanField";
-            this.tpgScanField.Size = new System.Drawing.Size(230, 667);
+            this.tpgScanField.Size = new System.Drawing.Size(230, 670);
             this.tpgScanField.TabIndex = 1;
             this.tpgScanField.Text = "Scan Field";
             // 
-            // dockMain
+            // statusStrip
             // 
-            this.dockMain.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.dockMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.dockMain.CanCloseTabs = true;
-            this.dockMain.CanMoveTabs = true;
-            this.dockMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dockMain.Location = new System.Drawing.Point(0, 49);
-            this.dockMain.Name = "dockMain";
-            this.dockMain.Size = new System.Drawing.Size(751, 552);
-            this.dockMain.TabIndex = 7;
-            this.dockMain.TabsSpacing = 0;
+            this.statusStrip.Location = new System.Drawing.Point(0, 739);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(984, 22);
+            this.statusStrip.TabIndex = 10;
+            this.statusStrip.Text = "statusStrip1";
             // 
             // FormMain
             // 
@@ -190,25 +222,27 @@
             this.Controls.Add(this.dockMain);
             this.Controls.Add(this.dockBottom);
             this.Controls.Add(this.dockLeft);
-            this.Controls.Add(this.toolStrip);
             this.Controls.Add(this.statusStrip);
-            this.Controls.Add(this.menuStrip);
+            this.Controls.Add(this.dockToolBar);
+            this.Controls.Add(this.mainMenu);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MainMenuStrip = this.menuStrip;
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Z1 Confocal v";
-            ((System.ComponentModel.ISupportInitialize)(this.dockLeft)).EndInit();
-            this.dockLeft.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.cmdHolder)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dockToolBar)).EndInit();
+            this.dockToolBar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dockMain)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dockBottom)).EndInit();
             this.dockBottom.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dockTabOutput)).EndInit();
             this.dockTabOutput.ResumeLayout(false);
             this.tpgLog.ResumeLayout(false);
             this.tpgLog.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dockLeft)).EndInit();
+            this.dockLeft.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dockTabScan)).EndInit();
             this.dockTabScan.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dockMain)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -216,17 +250,21 @@
 
         #endregion
 
-        private System.Windows.Forms.StatusStrip statusStrip;
-        private System.Windows.Forms.MenuStrip menuStrip;
-        private System.Windows.Forms.ToolStrip toolStrip;
-        private C1.Win.C1Command.C1CommandDock dockLeft;
+        private C1.Win.C1Command.C1MainMenu mainMenu;
+        private C1.Win.C1Command.C1CommandHolder cmdHolder;
+        private C1.Win.C1Command.C1DockingTab dockMain;
         private C1.Win.C1Command.C1CommandDock dockBottom;
         private C1.Win.C1Command.C1DockingTab dockTabOutput;
         private C1.Win.C1Command.C1DockingTabPage tpgLog;
         private System.Windows.Forms.TextBox textLog;
+        private C1.Win.C1Command.C1CommandDock dockLeft;
         private C1.Win.C1Command.C1DockingTab dockTabScan;
         private C1.Win.C1Command.C1DockingTabPage tpgScanSettings;
         private C1.Win.C1Command.C1DockingTabPage tpgScanField;
-        private C1.Win.C1Command.C1DockingTab dockMain;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private C1.Win.C1Command.C1CommandDock dockToolBar;
+        private C1.Win.C1Command.C1ToolBar toolBar;
+        private C1.Win.C1Command.C1CommandLink c1CommandLink2;
+        private C1.Win.C1Command.C1CommandLink c1CommandLink1;
     }
 }
