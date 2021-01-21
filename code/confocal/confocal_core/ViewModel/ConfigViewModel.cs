ea70d786f0535bb2a09b552aff5497cc1411cfda
tangmb757 <tangmb@zhejianglab.com>
2021-01-21 23:11:07 +0800

    update

---
+++ code/confocal/confocal_core/ViewModel/ConfigViewModel.cs
@@ -0,0 +1,332 @@
+﻿using confocal_core.Model;
+using confocal_core.Properties;
+using GalaSoft.MvvmLight;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace confocal_core.ViewModel
+{
+    public class ConfigViewModel : ViewModelBase
+    {
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        private static readonly ILog Logger = LogManager.GetLogger("info");
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        private ScanAcquisitionModel scanLiveMode;
+        private ScanAcquisitionModel scanCaptureMode;
+
+        /// <summary>
+        /// 实时模式
+        /// </summary>
+        public ScanAcquisitionModel ScanLiveMode
+        {
+            get { return scanLiveMode; }
+            set { scanLiveMode = value; RaisePropertyChanged(() => ScanLiveMode); }
+        }
+        /// <summary>
+        /// 捕捉模式
+        /// </summary>
+        public ScanAcquisitionModel ScanCaptureMode
+        {
+            get { return scanCaptureMode; }
+            set { scanCaptureMode = value; RaisePropertyChanged(() => ScanCaptureMode); }
+        }
+
+        /// <summary>
+        /// 扫描状态
+        /// </summary>
+        public bool IsScanning
+        {
+            get { return ScanLiveMode.IsEnabled || ScanCaptureMode.IsEnabled; }
+        }
+        /// <summary>
+        /// 当前的采集模式
+        /// </summary>
+        public ScanAcquisitionModel SelectedScanAcquisition
+        {
+            get
+            {
+                if (!IsScanning)
+                {
+                    return null;
+                }
+                return ScanLiveMode.IsEnabled ? ScanLiveMode : ScanCaptureMode;
+            }
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        private ScannerHeadModel scannerHeadTwoGalv;
+        private ScannerHeadModel scannerHeadThreeGalv;
+
+        /// <summary>
+        /// 双镜
+        /// </summary>
+        public ScannerHeadModel ScannerHeadTwoGalv
+        {
+            get { return scannerHeadTwoGalv; }
+            set { scannerHeadTwoGalv = value; RaisePropertyChanged(() => ScannerHeadTwoGalv); }
+        }
+        /// <summary>
+        /// 三镜
+        /// </summary>
+        public ScannerHeadModel ScannerHeadThreeGalv
+        {
+            get { return scannerHeadThreeGalv; }
+            set { scannerHeadThreeGalv = value; RaisePropertyChanged(() => ScannerHeadThreeGalv); }
+        }
+        /// <summary>
+        /// 选择的扫描头
+        /// </summary>
+        public ScannerHeadModel SelectedScannerHead
+        {
+            get { return ScannerHeadTwoGalv.IsEnabled ? ScannerHeadTwoGalv : ScannerHeadThreeGalv; }
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        private ScanDirectionModel scanUniDirection;
+        private ScanDirectionModel scanBiDirection;
+
+        /// <summary>
+        /// 选择的扫描方向
+        /// </summary>
+        public ScanDirectionModel SelectedScanDirection
+        {
+            get { return ScanUniDirection.IsEnabled ? ScanUniDirection : ScanBiDirection; }
+        }
+        /// <summary>
+        /// 单向
+        /// </summary>
+        public ScanDirectionModel ScanUniDirection
+        {
+            get { return scanUniDirection; }
+            set { scanUniDirection = value; RaisePropertyChanged(() => ScanUniDirection); }
+        }
+        /// <summary>
+        /// 双向
+        /// </summary>
+        public ScanDirectionModel ScanBiDirection
+        {
+            get { return scanBiDirection; }
+            set { scanBiDirection = value; RaisePropertyChanged(() => ScanBiDirection); }
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        private ScanModeModel scanModeGalvano;
+        private ScanModeModel scanModeResonant;
+
+        /// <summary>
+        /// 选择的扫描模式
+        /// </summary>
+        public ScanModeModel SelectedScanMode
+        {
+            get { return ScanModeGalavano.IsEnabled ? ScanModeGalavano : ScanModeResonant; }
+        }
+        /// <summary>
+        /// Galvano扫描模式
+        /// </summary>
+        public ScanModeModel ScanModeGalavano
+        {
+            get { return scanModeGalvano; }
+            set { scanModeGalvano = value; RaisePropertyChanged(() => ScanModeGalavano); }
+        }
+        /// <summary>
+        /// Resonant扫描模式
+        /// </summary>
+        public ScanModeModel ScanModeResonant
+        {
+            get { return scanModeResonant; }
+            set { scanModeResonant = value; RaisePropertyChanged(() => ScanModeResonant); }
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        private List<ScanPixelModel> scanPixelList;
+
+        /// <summary>
+        /// 扫描像素列表
+        /// </summary>
+        public List<ScanPixelModel> ScanPixelList
+        {
+            get { return scanPixelList; }
+            set { scanPixelList = value; RaisePropertyChanged(() => ScanPixelList); }
+        }
+        /// <summary>
+        /// 选择的扫描像素 
+        /// </summary>
+        public ScanPixelModel SelectedScanPixel
+        {
+            get { return ScanPixelList.Where(p => p.IsEnabled).First(); }
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        private bool fastModeEnabled;
+        private List<ScanPixelDwellModel> scanPixelDwellList;
+
+        /// <summary>
+        /// 快速模式使能
+        /// </summary>
+        public bool FastModeEnabled
+        {
+            get { return fastModeEnabled; }
+            set { fastModeEnabled = value; RaisePropertyChanged(() => FastModeEnabled); }
+        }
+        /// <summary>
+        /// 像素停留时间列表
+        /// </summary>
+        public List<ScanPixelDwellModel> ScanPixelDwellList
+        {
+            get { return scanPixelDwellList; }
+            set { scanPixelDwellList = value; RaisePropertyChanged(() => ScanPixelDwellList); }
+        }
+        /// <summary>
+        /// 选择的像素停留时间
+        /// </summary>
+        public ScanPixelDwellModel SelectedScanPixelDwell
+        {
+            get { return scanPixelDwellList.Where(p => p.IsEnabled).First(); }
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        private bool scanLineSkipEnabled;
+        private ScanLineSkipModel selectedScanLineSkip;
+        private List<ScanLineSkipModel> scanLineSkipList;
+
+        /// <summary>
+        /// 跳行扫描使能
+        /// </summary>
+        public bool ScanLineSkipEnabled
+        {
+            get { return scanLineSkipEnabled; }
+            set { scanLineSkipEnabled = value; RaisePropertyChanged(() => ScanLineSkipEnabled); }
+        }
+        /// <summary>
+        /// 选择的跳行扫描
+        /// </summary>
+        public ScanLineSkipModel SelectedScanLineSkip
+        {
+            get { return selectedScanLineSkip; }
+            set { selectedScanLineSkip = value; RaisePropertyChanged(() => SelectedScanLineSkip); }
+        }
+        /// <summary>
+        /// 跳行扫描列表
+        /// </summary>
+        public List<ScanLineSkipModel> ScanLineSkipList
+        {
+            get { return scanLineSkipList; }
+            set { scanLineSkipList = value; RaisePropertyChanged(() => SelectedScanLineSkip); }
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        private ScanChannelModel scanChannel405;
+        private ScanChannelModel scanChannel488;
+        private ScanChannelModel scanChannel561;
+        private ScanChannelModel scanChannel640;
+
+        /// <summary>
+        /// 405nm通道
+        /// </summary>
+        public ScanChannelModel ScanChannel405
+        {
+            get { return scanChannel405; }
+            set { scanChannel405 = value; RaisePropertyChanged(() => ScanChannel405); }
+        }
+        /// <summary>
+        /// 488nm通道
+        /// </summary>
+        public ScanChannelModel ScanChannel488
+        {
+            get { return scanChannel488; }
+            set { scanChannel488 = value; RaisePropertyChanged(() => ScanChannel488); }
+        }
+        /// <summary>
+        /// 561nm通道
+        /// </summary>
+        public ScanChannelModel ScanChannel561
+        {
+            get { return scanChannel561; }
+            set { scanChannel561 = value; RaisePropertyChanged(() => ScanChannel561); }
+        }
+        /// <summary>
+        /// 640nm通道
+        /// </summary>
+        public ScanChannelModel ScanChannel640
+        {
+            get { return scanChannel640; }
+            set { scanChannel640 = value; RaisePropertyChanged(() => ScanChannel640); }
+        }
+
+        private ScanChannelModel FindScanChannel(int id)
+        {
+            switch (id)
+            {
+                case 0:
+                    return ScanChannel405;
+                case 1:
+                    return ScanChannel488;
+                case 2:
+                    return ScanChannel561;
+                case 3:
+                    return ScanChannel640;
+                default:
+                    throw new ArgumentOutOfRangeException("ID Exception.");
+            }
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        private List<ScanPinHoleModel> scanPinHoleList;
+        private ScanPinHoleModel selectedPinHole;
+
+        /// <summary>
+        /// 小孔列表
+        /// </summary>
+        public List<ScanPinHoleModel> ScanPinHoleList
+        {
+            get { return scanPinHoleList; }
+            set { scanPinHoleList = value; RaisePropertyChanged(() => ScanPinHoleList); }
+        }
+        /// <summary>
+        /// 选择的小孔
+        /// </summary>
+        public ScanPinHoleModel SelectedPinHole
+        {
+            get { return selectedPinHole; }
+            set { selectedPinHole = value; RaisePropertyChanged(() => SelectedPinHole); }
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        public ConfigViewModel()
+        {
+            // 采集模式
+            ScanLiveMode = ScanAcquisitionModel.Initialize(ScanAcquisitionModel.LIVE);
+            ScanCaptureMode = ScanAcquisitionModel.Initialize(ScanAcquisitionModel.CAPTURE);
+            // 扫描头
+            ScannerHeadTwoGalv = ScannerHeadModel.Initialize(ScannerHeadModel.TWO_SCANNERS);
+            ScannerHeadThreeGalv = ScannerHeadModel.Initialize(ScannerHeadModel.THREE_SCANNERS);
+            // 扫描模式
+            ScanModeResonant = ScanModeModel.Initialize(ScanModeModel.RESONANT);
+            ScanModeGalavano = ScanModeModel.Initialize(ScanModeModel.GALVANO);
+            // 扫描方向
+            ScanUniDirection = ScanDirectionModel.Initialize(ScanDirectionModel.UNIDIRECTION);
+            ScanBiDirection = ScanDirectionModel.Initialize(ScanDirectionModel.BIDIRECTION);
+            // 像素时间
+            FastModeEnabled = false;
+            ScanPixelDwellList = ScanPixelDwellModel.Initialize();
+            // 扫描像素
+            ScanPixelList = ScanPixelModel.Initialize();
+            // 跳行扫描
+            ScanLineSkipEnabled = Settings.Default.ScanLineSkipEnabled;
+            ScanLineSkipList = ScanLineSkipModel.Initialize();
+            SelectedScanLineSkip = ScanLineSkipList.Where(p => p.ID == Settings.Default.ScanLineSkip).First();
+            // 扫描通道
+            ScanChannel405 = ScanChannelModel.Initialize(ScanChannelModel.CHANNEL405);
+            ScanChannel488 = ScanChannelModel.Initialize(ScanChannelModel.CHANNEL488);
+            ScanChannel561 = ScanChannelModel.Initialize(ScanChannelModel.CHANNEL561);
+            ScanChannel640 = ScanChannelModel.Initialize(ScanChannelModel.CHANNEL640);
+            // 小孔
+            ScanPinHoleList = ScanPinHoleModel.Initialize();
+            SelectedPinHole = ScanPinHoleList[0];
+        }
+
+    }
+}
