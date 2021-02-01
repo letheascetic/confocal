ce186e142a0da623ec545f96b7cc615cecee0cd7
letheascetic <yizhifight@163.com>
2021-02-01 21:49:07 +0800

    update

---
+++ code/confocal/confocal_util/Matrix.cs
@@ -0,0 +1,18 @@
+﻿using Emgu.CV;
+using NumSharp;
+using System;
+
+namespace confocal_util
+{
+    public class Matrix
+    {
+
+        public static Mat ConvertToMatrix(ushort[] samples, int samplesPerPixel, int samplesPerRow, int sampleCount)
+        {
+            var nd = np.array(samples);
+            return new Mat();
+        }
+
+
+    }
+}
